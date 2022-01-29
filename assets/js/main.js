65c83e0c3bd1c33eba4a7043d2d35758c5ea48fc
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-01-29 23:56:12 +0600

    Header and Nav Menu & Home

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,35 @@
-/*=============== SHOW MENU ===============*/
+// =============== SHOW MENU ===============
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
+// ======== MENU SHOW ========
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
 
-/*=============== REMOVE MENU MOBILE ===============*/
+// ======== MENU HIDDEN ========
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
+// =============== REMOVE MENU MOBILE ===============
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  navMenu.classList.remove("show-menu");
+}
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
-
-/*==================== SHOW SCROLL UP ====================*/ 
-
+/*==================== SHOW SCROLL UP ====================*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-
